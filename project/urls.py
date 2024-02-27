e6668b5f30dfbdf1563f1a92d0369507c0c48af8
Ruan C4rlos <ruancarlos@ufpi.edu.br>
2024-02-27 16:16:42 -0300

    Realizando testes unitários

--- project/urls.py
+++ project/urls.py
@@ -13,11 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from django.conf.urls.static import static
-from django.conf import settings
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +24,4 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- recipes/tests.py
+++
@@ -1,6 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-class RecipeURLsText(TestCase):
-    def test_th_pytest_is_ok(self):
-        ...
